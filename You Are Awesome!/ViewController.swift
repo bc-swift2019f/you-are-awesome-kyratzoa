b62130a53594a4c3a4534489b26df03e351a5d78
Anastasia Kyratzoglou <kyratzoa@bc.edu>
2019-09-01 13:40:19 -0400

    2.3 Completed - Buttons, Labels, Linking the Interface Elements to Code

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    //Code below executes when the app's view first loads
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-
-
+    
+    @IBAction func showMessagePress(_ sender: UIButton) {
+        messageLabel.text = "You are Awesome!"
+    }
 }
 
